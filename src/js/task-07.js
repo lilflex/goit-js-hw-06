b359a3b881ed05fc3ac5f1aac2c19c578df5a44f
Aleksej <kralinaleksej@gmail.com>
2023-01-05 22:33:21 +0200

    HW06

---
+++ src/js/task-07.js
@@ -0,0 +1,6 @@
+const inputChange = document.querySelector('input#font-size-control')
+const abracadabra = document.querySelector('span#text')
+
+inputChange.addEventListener('change', event => {
+    abracadabra.style.fontSize = `${event.target.value }px`
+})
